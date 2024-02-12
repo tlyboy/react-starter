db5bff838c4667432dae9b58a554d988cabb37ef
Guany <1084072690@qq.com>
2024-02-12 11:49:43 +0800

    feat: add unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import AutoImport from 'unplugin-auto-import/vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     AutoImport({
       imports: ['react'],
       dirs: [
         'src/utils',
       ],
     }),
+    react(),
   ],
   resolve: {
     alias: {
