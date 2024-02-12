48a07210168e196287ae53051d7d81787f9cb231
Guany <1084072690@qq.com>
2024-02-12 11:43:52 +0800

    feat: add unplugin-auto-import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 function App() {
   const [count, setCount] = useState(0)
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({
+      imports: ['react'],
+      dirs: [
+        'src/utils',
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
