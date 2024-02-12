b11571ca915bd9a1054ea156dd974bf92fee1084
Guany <1084072690@qq.com>
2024-02-12 14:11:08 +0800

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div h-full flex="~ col" items-center justify-center>
-        <button btn onClick={() => setCount(count + 1)}>
-          +
-          {' '}
-          {count}
-        </button>
-      </div>
+      <main
+        px-4
+        py-10
+        text="center gray-700 dark:gray-200"
+      >
+        <div>
+          <p>
+            🚀 React starter
+          </p>
+        </div>
+        <nav mt-6 flex justify-center gap-2 text-xl>
+          <a icon-btn href="https://github.com/tlyboy/react-starter" target="_blank" rel="noopener noreferrer" title="GitHub">
+            <div i-carbon-logo-github />
+          </a>
+        </nav>
+      </main>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import '@unocss/reset/tailwind.css'
 import './assets/styles/main.css'
 import 'uno.css'
+import nProgress from 'nprogress'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+nProgress.start()
+nProgress.done()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
