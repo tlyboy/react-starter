789e43c58ccb2119925ec1219f781ced5ca83920
Guany <1084072690@qq.com>
2024-02-21 15:24:36 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -3,13 +3,15 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
 } from 'unocss'
 import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-hex-3498db text-white cursor-pointer hover:bg-hex-2980b9 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-hex-3498db !outline-none'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-hex-3498db text-white cursor-pointer !outline-none hover:bg-hex-2980b9 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-hex-3498db'],
   ],
   presets: [
     presetUno(),
@@ -19,6 +21,8 @@ export default defineConfig({
     }),
   ],
   transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
     transformerAttributifyJsx(),
   ],
 })
