11e9520d44899347be5e7d2aeafa69bcd18027cd
Guany <1084072690@qq.com>
2025-08-03 12:41:59 +0800

    更新依赖项，将 @vitejs/plugin-react-swc 替换为 @vitejs/plugin-react-oxc，并将 vite 版本更改为 rolldown-vite@latest。同时更新 pnpm-lock.yaml 文件以反映这些更改。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-oxc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
