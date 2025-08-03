88bb8bf864b6e704359453cc03571d147f68ce0c
Guany <1084072690@qq.com>
2025-08-03 16:08:38 +0800

    更新依赖项，添加 immer 和 use-immer，并重构 App 组件以使用新的 Default 布局组件。

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { ThemeProvider } from '@/components/theme-provider'
-import { ModeToggle } from './components/mode-toggle'
+import Default from './layouts/default'
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex h-full flex-col items-center justify-center gap-4">
-        <div>React starter</div>
-
-        <div className="flex items-center gap-2 text-xl">
-          <a
-            className="i-carbon-logo-github icon-btn"
-            href="https://github.com/tlyboy/react-starter"
-            target="_blank"
-            rel="noopener noreferrer"
-          ></a>
-
-          <ModeToggle />
-        </div>
-      </div>
-    </ThemeProvider>
+    <Default>
+      <div>React starter</div>
+    </Default>
   )
 }
 
